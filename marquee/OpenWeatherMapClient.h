c1a04ebcf842421a2842198558c4dac933c48090
idezentas <idezentas@gmail.com>
2023-11-05 03:18:37 +0300

    Update OpenWeatherMapClient.h

--- marquee/OpenWeatherMapClient.h
+++ marquee/OpenWeatherMapClient.h
@@ -1,6 +1,9 @@
+#include "WString.h"
 /** The MIT License (MIT)
 
 Copyright (c) 2018 David Payne
+
+Air Quality and Name Search added by idezentas
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -23,27 +26,34 @@ SOFTWARE.
 
 #pragma once
 #include <ESP8266WiFi.h>
-#include "libs/ArduinoJson/ArduinoJson.h"
+#include <ArduinoJson.h>
+#include <TimeLib.h>
 
-class OpenWeatherMapClient {
+class OpenWeatherMapClient
+{
 
 private:
-  String myCityIDs = "";
   String myApiKey = "";
+  String myLang = "";
   String units = "";
-  
-  const char* servername = "api.openweathermap.org";  // remote server we will connect to
+  String myCityName = "";
+  String myWorldCityName = "";
 
-  typedef struct {
+  const char *servername = "api.openweathermap.org"; // remote server we will connect to
+
+  typedef struct
+  {
     String lat;
     String lon;
     String dt;
+    String id;
     String city;
     String country;
     String temp;
     String humidity;
     String condition;
     String wind;
+    String gust;
     String weatherId;
     String description;
     String icon;
@@ -54,25 +64,45 @@ private:
     String high;
     String low;
     String timeZone;
+    String feel;
+    String sunRise;
+    String sunSet;
+    String cloudcover;
+    String aqi;
+    String co;
+    String no;
+    String no2;
+    String o3;
+    String so2;
+    String pm2_5;
+    String pm10;
+    String nh3;
   } weather;
 
-  weather weathers[5];
+  weather weathers[10];
 
   String roundValue(String value);
-  
+  String zeroPad(int number);
+
 public:
-  OpenWeatherMapClient(String ApiKey, int CityIDs[], int cityCount, boolean isMetric);
-  void updateWeather();
+  OpenWeatherMapClient(String ApiKey, boolean isMetric, String CityName, String language);
+  void updateWeatherName(String CityName, int index);
+  void updateCityAirPollution(String latitude, String longitude, int index);
   void updateWeatherApiKey(String ApiKey);
-  void updateCityIdList(int CityIDs[], int cityCount);
   void setMetric(boolean isMetric);
+  void updateCityName(String CityName);
+  void updateWorldCityName(String WorldCityName);
+  void updateLanguage(String language);
 
   String getLat(int index);
   String getLon(int index);
   String getDt(int index);
+  String getID(int index);
   String getCity(int index);
   String getCountry(int index);
   String getTemp(int index);
+  String getFeel(int index);
+  String getFeelRounded(int index);
   String getTempRounded(int index);
   String getHumidity(int index);
   String getHumidityRounded(int index);
@@ -84,14 +114,31 @@ public:
   String getDirectionText(int index);
   String getPressure(int index);
   String getHigh(int index);
+  String getHighRounded(int index);
   String getLow(int index);
+  String getLowRounded(int index);
   String getWeatherId(int index);
   String getDescription(int index);
   String getIcon(int index);
   boolean getCached();
-  String getMyCityIDs();
+  String getMyCityName();
   String getWeatherIcon(int index);
   String getError();
   String getWeekDay(int index, float offset);
   int getTimeZone(int index);
+  String getCloudcover(int index);
+  String getSeaLevel(int index);
+  String getGrndLevel(int index);
+  String getSunrise(int index);
+  String getSunset(int index);
+  String cleanText(String text);
+  String getAqi(int index);
+  String getCO(int index);
+  String getNO(int index);
+  String getNO2(int index);
+  String getO3(int index);
+  String getSO2(int index);
+  String getPM10(int index);
+  String getPM2_5(int index);
+  String getNH3(int index);
 };
