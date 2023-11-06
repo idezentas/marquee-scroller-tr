9c713c2d47bfe241d74632b78b610c9dd074c921
idezentas <idezentas@gmail.com>
2023-11-06 17:10:49 +0300

    Update OpenWeatherMapClient.h

--- marquee/OpenWeatherMapClient.h
+++ marquee/OpenWeatherMapClient.h
@@ -39,6 +39,7 @@ private:
   String myCityName = "";
   String myWorldCityName1 = "";
   String myWorldCityName2 = "";
+  String myWorldCityName3 = "";
 
   const char *servername = "api.openweathermap.org"; // remote server we will connect to
 
@@ -94,6 +95,7 @@ public:
   void updateCityName(String CityName);
   void updateWorldCityName1(String WorldCityName1);
   void updateWorldCityName2(String WorldCityName2);
+  void updateWorldCityName3(String WorldCityName3);
   void updateLanguage(String language);
 
   String getLat(int index);
